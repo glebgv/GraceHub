7d1b62ca8d9d36e916e04de4b067b2b89fc296c2
Gleb Gribachyov <nexustetusgars@gmail.com>
2026-01-04 17:46:27 +0300

    обновил тени на модалке firstLauch

--- frontend/miniapp_frontend/src/pages/FirstLaunch.tsx
+++ frontend/miniapp_frontend/src/pages/FirstLaunch.tsx
@@ -489,9 +489,23 @@ const FirstLaunch: React.FC<FirstLaunchProps> = ({
         </div>
       </div>
 
-      {/* AddBotModal остается как есть - его обновим на следующем этапе */}
+      {/* AddBotModal с оверлеем для затемнения фона */}
       {showAddModal && (
-        <AddBotModal onClose={() => setShowAddModal(false)} onSubmitToken={handleSubmitToken} />
+        <>
+          <div
+            style={{
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: 'rgba(0, 0, 0, 0.4)',
+              zIndex: 9998,
+            }}
+            onClick={() => setShowAddModal(false)}
+          />
+          <AddBotModal onClose={() => setShowAddModal(false)} onSubmitToken={handleSubmitToken} />
+        </>
       )}
     </div>
   );
